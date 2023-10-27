c6d2d950b44ddfbfd10d5d24d457e54a94eec0bf
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-10-27 09:49:58 +0300

    added mongoose schema

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const ticketSchema =
+  ({
+    title: String,
+    desc: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    isActive: Boolean,
+  },
+  { timestamps: true });
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;
